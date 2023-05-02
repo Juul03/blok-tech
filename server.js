690baad3b589254fbeef646bde70a0c65c2ffe12
Juul03 <juliahoek16@gmail.com>
2023-05-02 14:17:29 +0200

    gitignore node_modules

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+// de static map openbaar maken (middleware)
+app.use(express.static('static'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World, Rainbows and sunshine!')
+  res.send('index')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('/about/:plant/', (req, res) => {
+  console.log('trying to reach' + plant)
+  res.params.send('hallo' + plant)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Shaking my booty ${port} times`)
 })
