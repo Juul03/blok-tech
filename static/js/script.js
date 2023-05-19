15e9233179aa06adf240abfa7d2ef4a2edd1d940
Juul03 <juliahoek16@gmail.com>
2023-05-19 16:38:03 +0200

    quote api fixed

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,29 @@
 console.log('test we zijn in leven');
 
+const quoteContainer = document.querySelector('main > q');
+
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://api.goprogram.ai/inspiration');
+        console.log(response);
+        var data = await response.json();
+        console.log(data);
+        console.log(data.quote);
+
+        let quoteOfTheHour = data.quote;
+        console.log(quoteOfTheHour);
+        quoteContainer.innerHTML = quoteOfTheHour;
+  }
+    catch{
+        console.log('error')
+    }
+  }
+
+  fetchData();
+
+ 
+
+
 // const soilTypeOptionList = document.querySelector('#soil-type');
 // let value = soilTypeOptionList.value;
 // let selectedSoilType = soilTypeOptionList[soilTypeOptionList.selectedIndex].text;
