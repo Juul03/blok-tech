e6d0af9c87afe5ae4c817805d6cbc3d05c3e58f6
Juul03 <juliahoek16@gmail.com>
2023-05-23 12:01:54 +0200

    custom validation form

--- static/js/formscript.js
+++ static/js/formscript.js
@@ -5,17 +5,64 @@ const imgInput = document.querySelector('form > div:first-of-type > input:first-
 const imgOutput = document.querySelector('form > div:first-of-type > output');
 let plantImages = [];
 
+//form inputs
+const heightInput = document.querySelector('form > input[type=number]');
+const submitButton = document.querySelector('form button[type=submit]');
+
+const validateHeightInput = () => {
+    const input = heightInput;
+    const validityState = input.validity;
+
+    if(validityState.valueMissing) {
+        console.log('niet ingevuld hoogte');
+        input.setCustomValidity("Please fill in the height of your plant");
+    } else if(validityState.badInput) {
+        console.log('pls enter number, no letters');
+    } else {
+        input.setCustomValidity("");
+        console.log('niks aan de hand');
+    }
+
+    input.reportValidity();
+}
+
+//Input checkt elke keer als er wat verandert in het input veld, Change checkt elke keer als er er niet meer gefocused wordt op de input
+heightInput.addEventListener('input', validateHeightInput);
+//Ook valideren als het formulier gesubmit wordt
+submitButton.addEventListener('click', validateHeightInput)
+
+// function validate(heightInput) {
+//     const input = document.getElementById(inputID);
+//     const validityState = input.validity;
+  
+//     if (validityState.valueMissing) {
+//       input.setCustomValidity("You gotta fill this out, yo!");
+//     } else if (validityState.rangeUnderflow) {
+//       input.setCustomValidity("We need a higher number!");
+//     } else if (validityState.rangeOverflow) {
+//       input.setCustomValidity("Thats too high!");
+//     } else {
+//       input.setCustomValidity("");
+//     }
+  
+//     input.reportValidity();
+// }
+
 //form input field appear
 const checkboxCustomSoil= document.querySelector('#else')
 const inputfieldCustomSoil = document.querySelector('fieldset input[type=text]');
 
 console.log(checkboxCustomSoil.checked);
+
+inputfieldCustomSoil.classList.add('hidden');
+
 const inputFieldAppear = () => {
     if(checkboxCustomSoil.checked) {
         console.log('it is checked');
-        inputfieldCustomSoil.classList.add('active');
+        inputfieldCustomSoil.classList.remove('hidden');
     } else {
-        inputfieldCustomSoil.classList.remove('active');
+        console.log('it is not checked');
+        inputfieldCustomSoil.classList.add('hidden');
     }
 }
 
