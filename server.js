60ac4fc7844fe4642f5e7ec9c7b4447f0e89fb2d
Juul03 <juliahoek16@gmail.com>
2023-05-31 17:05:39 +0200

    deleted dead code

--- server.js
+++ server.js
@@ -18,16 +18,20 @@ const client = new MongoClient(uri, {
   }
 });
 
-// de static map openbaar maken (middleware)
+// Making the static map public (middleware)
 app.use(express.static('static'));
 
-// View engine instellen en server laten weten welk mapje deze staat
+// Used view engine and letting the server know where to find the files
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// formulier middleware
+// Form (middleware)
 app.use(express.urlencoded({extended: true}));
 
+// Multer: where to save uploaded files
+const upload = multer({ dest: './static/uploadedplantimg' })
+
+// MongoDB connection
 const mongoDBRun = async (req, res, next) => {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -45,25 +49,7 @@ const mongoDBRun = async (req, res, next) => {
 mongoDBRun().catch(console.dir);
 
 
-const dataPlant = (req, res) => {
-  var id = slug(req.body.name).toLowerCase();
-
-  dataPlant.push({
-    id: id,
-    name: req.body.name
-  });
-
-  console.log(req.body.name);
-  res.redirect('/' + id);
-} 
-
-var plants = [
-  { planttype: 'Spiderplant', height:'25', repot:'yes'},
-  { planttype: 'Cactus', height:'100', repot:'no'},
-  { planttype: 'Calathea', height:'14', repot:'no'}
-];
-
-// ROUTES
+// ROUTES get request
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'home',
@@ -77,15 +63,36 @@ app.get('/upload', (req, res) => {
   })
 })
 
+app.get('/feed', async (req, res) => {
+  const plantCollection = client
+          .db('Testbase')
+          .collection('Test1')
 
-// Multer: where to save uploaded files
-const upload = multer({ dest: './static/uploadedplantimg' })
+    const AllPlantData = await plantCollection.find({}).toArray();
+    console.log(AllPlantData);
 
+    res.render('feed.ejs', {
+        title: 'plantfeed',
+        AllPlantData: AllPlantData,
+    })
+})
+
+app.get('/succes', (req, res) => {
+  res.render('succes', {
+    title: 'succes'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.status(404).render('not-found.ejs', {
+    title: '404'
+  });
+});
+
+
+// ROUTES post request
 app.post('/upload', upload.single('plantpic'), async (req, res) => {
-  console.log('testpost')
-  console.log(req.file)
 
-  // plantimages = req.file.filename;
   plantimg = req.file
   planttype = req.body.planttype;
   height = req.body.plantheight;
@@ -97,12 +104,10 @@ app.post('/upload', upload.single('plantpic'), async (req, res) => {
   carelevel = req.body.carelevel;
   additionalinfo = req.body.additionalinfo;
 
-  // Alle data opslaan in een object
   const formPlantData = {plantimg, planttype, height, potdiameter, repot, sunlight, water, soiltype, carelevel, additionalinfo}
 
   try {
     await sendData(formPlantData);
-    console.log(height);
     res.redirect('/succes')
   } catch(err) {
     console.error("Something went wrong with sending data to the db", err);
@@ -122,28 +127,8 @@ async function sendData(data) {
   }
 }
 
-app.get('/feed', async (req, res) => {
-  const plantCollection = client
-          .db('Testbase')
-          .collection('Test1')
 
-    const AllPlantData = await plantCollection.find({}).toArray();
-    console.log(AllPlantData);
-
-    res.render('feed.ejs', {
-        title: 'plantfeed',
-        AllPlantData: AllPlantData,
-        // planttype: AllPlantData.planttype
-    })
-})
-
-app.get('/succes', (req, res) => {
-  res.render('succes', {
-    title: 'succes'
-  })
-})
-
-
+// ROUTES bonus
 app.get('/feed/:plantType1', (req, res) => {
   res.render('plant.ejs', {
     plantType: req.params.plantType1,
@@ -151,45 +136,18 @@ app.get('/feed/:plantType1', (req, res) => {
   })
 })
 
-// app.get("/datatest", async (req, res) => {
-//   try {
-//       // Connect the client to the server (optional starting in v4.7)
-//       await client.connect();
+const dataPlant = (req, res) => {
+  var id = slug(req.body.name).toLowerCase();
 
-//       // Send a ping to confirm a successful connection
-//       await client.db("admin").command({ ping: 1 });
-//       console.log('Pinged your deployment. You successfully connected to MongoDB!');
-
-//       const collection = client
-//           .db('Testbase')
-//           .collection('Test1');
-
-//       const data = await collection.find().toArray();
-//       console.log(data);
-//       res.send(data);
-  
-//   } catch (err) {
-//       console.log(err);
-//   } finally {
-//       await client.close();
-//   }
-// });
-
-app.get('*', (req, res) => {
-  res.status(404).render('not-found.ejs', {
-    title: '404'
+  dataPlant.push({
+    id: id,
+    name: req.body.name
   });
-});
 
-// function plants(req,res) {
-//   res.render('index.ejs', {
-//     data: plants
-//   })
-// }
+  console.log(req.body.name);
+  res.redirect('/' + id);
+} 
 
-// app.get('/about', (req, res) => {
-//   res.send('About')
-// })
 
 app.listen(port, () => {
   console.log(`Shaking my booty ${port} times`)
