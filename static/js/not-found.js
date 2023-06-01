89db40a3f343722c9e7b3e4da31f37fa3605472d
Juul03 <juliahoek16@gmail.com>
2023-06-01 16:09:37 +0200

    meme api fixed

--- static/js/not-found.js
+++ static/js/not-found.js
@@ -2,11 +2,41 @@ console.log('sadly not found, but atleast the js is active');
 
 const memeContainer = document.querySelector('main > img');
 
-const fetchData = async () => {
+const fetchData2 = async () => {
+    try {
+        const response = await fetch('https://api.imgflip.com/get_memes');
+        const getMemes = await response.json();
+
+        const allMemesData = getMemes.data;
+        const allMemesArray = allMemesData.memes;
+
+        const getRandomMeme = (array) => {
+            const getRandomNumber = Math.floor(Math.random() * array.length);
+
+            const randomMeme = allMemesArray[getRandomNumber];
+            return randomMeme;
+        }
+        
+        const resultOfMemeGenerator = getRandomMeme(allMemesArray);
+
+        let memeUrl = resultOfMemeGenerator.url;
+        console.log(memeUrl);
+        memeContainer.src = memeUrl;      
+  }
+    catch{
+        console.log('error')
+    }
+  }
+
+fetchData2();
+
+
+
+  const fetchData = async () => {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon');
         const allPokemons = await response.json();
-        // console.log(allMemes.count);
+        console.log(allPokemons);
         const allPokemonsArray = allPokemons.results;
 
         console.log(allPokemonsArray);
